57285d3f4890fd874ee80c4e23d97be6e1c92c5b
Jake Brown <44589911+jaketbrown@users.noreply.github.com>
2023-04-21 22:09:59 -0400

    Update fuzz_verify.py

--- mayhem/fuzz_verify.py
+++ mayhem/fuzz_verify.py
@@ -3,14 +3,13 @@ import os.path
 
 import atheris
 import sys
-import datetime
 from random import random
 from cryptography.hazmat import backends
 from cryptography.hazmat.primitives.serialization import pkcs12
 
 with atheris.instrument_imports():
-    from endesive.pdf.cms import sign
-    from endesive.pdf import verify
+    #from endesive.pdf.cms import sign
+    #from endesive.pdf import verify
     from endesive.pdf import PyPDF2
 
 
@@ -19,14 +18,14 @@ def fuzz_test_verify(input_data):
     fdp = atheris.FuzzedDataProvider(input_data)
     try:
         with fdp.ConsumeBytes(fdp.remaining_bytes()) as f:
-            d = sign(f.encode('utf-8'),
-                         dct,
-                         p12[0],
-                         p12[1],
-                         p12[2],
-                         'sha256')
-            verify(d)
-            PyPDF2.PdfFileReader.read(d)
+            #d = sign(f.encode('utf-8'),
+            #             dct,
+            #             p12[0],
+            #             p12[1],
+            #             p12[2],
+            #             'sha256')
+            #verify(d)
+            PyPDF2.PdfFileReader.read(f.encode('utf-8'))
     except Exception:
         if random() > 0.99:
             raise
@@ -39,14 +38,14 @@ def main():
 
 
 if __name__ == "__main__":
-    path = os.path.dirname(os.path.abspath(__file__))
-    with open(path + '/demo2_user1.p12', 'rb') as fp:
-        p12 = pkcs12.load_key_and_certificates(fp.read(), b'1234', backends.default_backend())
-    dct = {
-        'sigflags': 3,
-        'contact': 'jake@mayhem.com',
-        'location': 'Elsewhere',
-        'signingdate': '01-01-2023',
-        'reason': 'For Mayhem',
-    }
+    #path = os.path.dirname(os.path.abspath(__file__))
+    #with open(path + '/demo2_user1.p12', 'rb') as fp:
+    #    p12 = pkcs12.load_key_and_certificates(fp.read(), b'1234', backends.default_backend())
+    #dct = {
+    #    'sigflags': 3,
+    #    'contact': 'jake@mayhem.com',
+    #    'location': 'Elsewhere',
+    #    'signingdate': '01-01-2023',
+    #    'reason': 'For Mayhem',
+    #}
     main()
