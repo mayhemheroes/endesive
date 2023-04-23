7671839df27b1938bc89ea59c553385f830e1122
jake <jake@MayhemVM.myguest.virtualbox.org>
2023-04-22 22:07:27 -0400

    Exclude all instrumentation except the library we care about

--- mayhem/fuzz_verify.py
+++ mayhem/fuzz_verify.py
@@ -3,13 +3,14 @@ import os.path
 
 import atheris
 import sys
+import io
 from random import random
 from cryptography.hazmat import backends
 from cryptography.hazmat.primitives.serialization import pkcs12
 
-with atheris.instrument_imports():
-    #from endesive.pdf.cms import sign
-    #from endesive.pdf import verify
+with atheris.instrument_imports(include=['endesive']):
+    # from endesive.pdf.cms import sign
+    # from endesive.pdf import verify
     from endesive.pdf import PyPDF2
 
 
@@ -18,19 +19,17 @@ def fuzz_test_verify(input_data):
     fdp = atheris.FuzzedDataProvider(input_data)
     try:
         with fdp.ConsumeBytes(fdp.remaining_bytes()) as f:
-          f = io.BytesIO(f)
-          yield f
-          f.close()
-        PyPDF2.PdfFileReader.read(f.encode('utf-8'))
+            f = io.BytesIO(f)
+        PyPDF2.PdfFileReader.read(f)
 
-            #d = sign(f.encode('utf-8'),
-            #             dct,
-            #             p12[0],
-            #             p12[1],
-            #             p12[2],
-            #             'sha256')
-            #verify(d)
-          
+        # d = sign(f.encode('utf-8'),
+        #             dct,
+        #             p12[0],
+        #             p12[1],
+        #             p12[2],
+        #             'sha256')
+        # verify(d)
+
     except Exception:
         if random() > 0.99:
             raise
@@ -43,14 +42,14 @@ def main():
 
 
 if __name__ == "__main__":
-    #path = os.path.dirname(os.path.abspath(__file__))
-    #with open(path + '/demo2_user1.p12', 'rb') as fp:
+    # path = os.path.dirname(os.path.abspath(__file__))
+    # with open(path + '/demo2_user1.p12', 'rb') as fp:
     #    p12 = pkcs12.load_key_and_certificates(fp.read(), b'1234', backends.default_backend())
-    #dct = {
+    # dct = {
     #    'sigflags': 3,
     #    'contact': 'jake@mayhem.com',
     #    'location': 'Elsewhere',
     #    'signingdate': '01-01-2023',
     #    'reason': 'For Mayhem',
-    #}
+    # }
     main()
