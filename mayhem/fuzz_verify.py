4017e646b4b61aa34b301af490874239e1334fb5
jake <jake@MayhemVM.myguest.virtualbox.org>
2023-04-27 23:15:40 -0400

    Try different error handling

--- mayhem/fuzz_verify.py
+++ mayhem/fuzz_verify.py
@@ -3,8 +3,6 @@ import os.path
 import atheris
 import sys
 
-#import random
-
 from cryptography.hazmat import backends
 from cryptography.hazmat.primitives.serialization import pkcs12
 
@@ -16,7 +14,7 @@ with atheris.instrument_imports(include=['endesive']):
 
 
 def TestOneInput(input_data):
-    global runs, p12, dct
+    global error_from_verify, error_from_enc, error_from_dec, runs, p12, dct
     fdp = atheris.FuzzedDataProvider(input_data)
     ran = fdp.ConsumeInt(fdp.ConsumeIntInRange(0, 4))
     runs += 1
@@ -29,23 +27,37 @@ def TestOneInput(input_data):
             hash_alg = 'sha384'
         else:
             hash_alg = 'sha512'
+
         consumed_bytes = fdp.ConsumeBytes(fdp.remaining_bytes())
         b = sign(consumed_bytes,
-                 dct,
-                 p12[0],
-                 p12[1],
-                 p12[2],
-                 hash_alg)
-        verify(b.decode('utf8'))
-        enc = encrypt(b, p12)
-        dec = decrypt(enc, p12)
-    except AssertionError:
-        return -1
+                 p12[0], p12[1], p12[2],
+                 hash_alg,
+                 attrs=False,
+                 pss=True
+                 )
+        # The functions will have an exception almost immediately when called
+        if runs > 1000:
+            if not error_from_verify:
+                try:
+                    verify(b.decode('utf-8'))
+                except UnicodeError:
+                    error_from_verify = True
+                    raise
+            if not error_from_enc:
+                try:
+                    encrypt(b, p12)
+                except AttributeError:
+                    error_from_enc = True
+                    raise
+            if not error_from_dec:
+                try:
+                    decrypt(b.decode('utf-8'), p12)
+                except AttributeError:
+                    error_from_dec = True
+                    raise
     except TypeError:
-        print(runs)
-        if runs > 10000:
-            raise
-        return -1
+        raise
+
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
@@ -53,6 +65,9 @@ def main():
 
 
 if __name__ == "__main__":
+    error_from_verify = False
+    error_from_dec = False
+    error_from_enc = False
     runs = 0
     path = os.path.dirname(os.path.abspath(__file__))
     with open(path + '/demo2_user1.p12', 'rb') as fp:
